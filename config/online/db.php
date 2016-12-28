5c6513fb9db05ae24dcab71629768e756c13c02f
caikang <290886367@qq.com>
2016-12-29 00:39:15 +0800

    config fix

--- config/online/db.php
+++ config/online/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=10.66.159.222;dbname=yii2basic',
+    'dsn' => 'mysql:host=10.66.159.222;dbname=jinlidb',
     'username' => 'root',
     'password' => 'ao5iuhdiuw3hr&_',
     'charset' => 'utf8',
