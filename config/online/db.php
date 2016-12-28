38d03786561c733bc22c88dfe61146ea12c126de
caikang <290886367@qq.com>
2016-12-29 00:00:33 +0800

    config fix

--- config/online/db.php
+++ config/online/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=10.66.159.222;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'ao5iuhdiuw3hr&_',
     'charset' => 'utf8',
 ];
--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-define('ENVIRONMENT', 'dev');
+define('ENVIRONMENT', 'online');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
